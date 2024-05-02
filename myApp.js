99a05454123920ddafc6fe0c323c487b6b2bbf2c
Gabriel De Castro <betterhalf278@gmail.com>
2024-05-02 08:30:18 +0000

    feat: Challenge 8

--- myApp.js
+++ myApp.js
@@ -1,5 +1,28 @@
-let express = require('express');
-let app = express();
+let express = require('express'); 
+require('dotenv').config(); 
+let app = express(); 
+const customMiddleware = function(req, res, next){ 
+    console.log(`${req.method} ${req.path} - ${req.ip}`) 
+    next(); 
+}; 
+app.use(customMiddleware); 
+app.use('/public', express.static(__dirname + '/public')); 
+app.get('/', function(req, res) { 
+    const path = __dirname + '/views/index.html'; res.sendFile(path); 
+}); 
+app.get('/json', function(req, res){ 
+    let message = 'Hello json'; 
+    if(process.env.MESSAGE_STYLE == 'uppercase'){ 
+        message = message.toUpperCase(); 
+    } 
+    res.send({"message": message}); 
+}); 
+app.get('/now', function(req, res, next){ 
+    req.time = new Date().toString(); 
+    next(); 
+}, function(req, res){ 
+    res.send({time: req.time}); 
+});
 
 
 
